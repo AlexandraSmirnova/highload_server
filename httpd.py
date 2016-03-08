b2022d3e0b1ac72ff1983223463a7deb1ed65992
AlexandriusTheFirst <smirnova.a.yu@gmail.com>
2016-03-08 16:19:39 +0300

    root_dir settings changed

--- httpd.py
+++ httpd.py
@@ -1,20 +1,19 @@
 import sys
+import os
 import asyncore
 import socket
 import re
 import urllib
 
-DOCUMENT_ROOT = sys.argv[2]
+DOCUMENT_ROOT = ''
 
 
 class Handler(asyncore.dispatcher_with_send):
     def handle_read(self):
         data = self.recv(8192)
         if data:
-            udata = data.decode('utf8')
-            result = re.findall(r'GET\s(/.+)\sHTTP', udata)
-            result = urllib.unquote(str(result))
-            response = self.find_file(result)
+            method, path = request_parser(data)
+            response = find_file(path)
 
             self.send('HTTP/1.0 ' + response['status'] + '\n')
             self.send('Content-Type: ' + response['type_res'] + ' \n')
@@ -22,23 +21,60 @@ class Handler(asyncore.dispatcher_with_send):
             self.send(response['file'])
             self.close()
 
-    def find_file(self, file_path):
-        full_path = DOCUMENT_ROOT + file_path[3: len(file_path) - 2]
-        type_res = ''
 
-        if len(re.findall(r'\.\w+$', full_path)) == 0:
-            full_path += '/index.html'
+class NotADirectoryError(object):
+    pass
 
-        try:
-            type_res = urllib.urlopen(full_path).info().type
-            my_file = open(full_path, 'r')
-            my_string = my_file.read()
-            my_file.close()
-            status = '200 OK'
-        except IOError:
-            my_string = "404 - Sorry we can't find your file"
-            status = '404 NOT FOUND'
-        return {'status': status, 'file': my_string, 'type_res': type_res}
+
+def set_document_root(root_dir):
+    global DOCUMENT_ROOT
+    if os.path.exists(root_dir):
+        if os.path.isdir(root_dir):
+            DOCUMENT_ROOT = root_dir
+        else:
+            raise OSError
+        print DOCUMENT_ROOT
+    else:
+        raise OSError
+
+
+def request_parser(request):
+    udata = request.decode('utf8')
+    pattern = '(GET|HEAD)\s(/.+)\sHTTP'
+    result = re.findall(pattern, udata)
+
+    if result.__len__() != 1:
+        return None, None
+    method = result[0][0]
+    path = result[0][1]
+    print method
+    print path
+    path = urllib.unquote(path)
+    return method, path
+
+
+def find_file(file_path):
+    if file_path is None:
+        file_path = '/'
+    full_path = DOCUMENT_ROOT + file_path
+    print full_path
+    type_res = ''
+
+    if len(re.findall(r'\.\w+$', full_path)) == 0:
+        full_path += '/index.html'
+
+    try:
+        if '..' in full_path:
+            raise IOError
+        type_res = urllib.urlopen(full_path).info().type
+        my_file = open(full_path, 'r')
+        my_string = my_file.read()
+        my_file.close()
+        status = '200 OK'
+    except IOError:
+        my_string = "404 - Sorry we can't find your file"
+        status = '404 NOT FOUND'
+    return {'status': status, 'file': my_string, 'type_res': type_res}
 
 
 class Server(asyncore.dispatcher):
@@ -70,6 +106,16 @@ class Server(asyncore.dispatcher):
 
 def main():
     try:
+        _r_flag = False
+        _c_flag = False
+        for param in sys.argv:
+            if _r_flag:
+                set_document_root(param)
+            if param == '-r':
+                _r_flag = True
+            if param == '-c':
+                _c_flag = True
+            continue
         print 'For server stopping press Ctrl^C '
         server = Server('127.0.0.1', 8080)
         asyncore.loop(0.1, True)
